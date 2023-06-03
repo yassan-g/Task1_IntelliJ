00375f6b0bc9b2112be0278faba7ec1dfbcade5a
yassan-g <exilev1courage@icloud.com>
2023-06-03 18:34:13 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 11; i++) {
+            if (i <= 10) {
+                System.out.println("[" +  i + "回目]" + "HelloWorld");
+            } else {
+                System.out.println("[" + i + "回目]" + "Hello");
+            }
+        }
+    }
+}
